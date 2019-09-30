9aa762efb7695177293e1b04a7180146a947a6b7
2ndPINEW <eito01178@gmail.com>
2019-09-30 20:12:31 +0900

    Delete - HelloWorld.vue,About.vue 各タイマー画面を移動

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import ButtleTimer from './components/ButtleTimer.vue';
-import WaitTimer from './components/WaitTimer.vue';
-import Setting from './components/Setting.vue';
+import ButtleTimer from './views/ButtleTimer.vue';
+import WaitTimer from './views/WaitTimer.vue';
+import Setting from './views/Setting.vue';
 
 Vue.use(Router)
 
